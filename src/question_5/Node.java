5d640e5fae09e01125f233c018948512395ab0fd
bsc24 <bchin08@gmail.com>
2020-04-03 02:12:28 -0400

    Create Node.java
    Created a Node class for question 5, it is slightly different from the Node classes of question 3 and question 4.

---
+++ src/question_5/Node.java
@@ -0,0 +1,87 @@
+package question_5;
+
+import java.util.HashMap;
+
+
+public class Node {
+	
+	private String value;
+	private boolean visited;
+	private int inDegree;
+	private HashMap<Node, Integer> neighbors;
+	
+	
+	public Node(String input) {
+		value = input;
+		visited = false;
+		neighbors = new HashMap<Node, Integer>();
+		inDegree = 0;
+	}
+	
+	
+	public String getValue() {
+		return value;
+	}
+	
+	
+	public HashMap<Node, Integer> getNeighbors() {
+		return neighbors;
+	}
+	
+	
+	public boolean isVisited() {
+		return visited;
+	}
+	
+	
+	public void setVisited() {
+		visited = true;
+	}
+	
+	
+	public void setUnvisited() {
+		visited = false;
+	}
+	
+	
+	public void setNeighbor(Node neighborNode, int weight) {
+		if (!neighbors.containsKey(neighborNode)) {
+			neighborNode.inDegree++;
+		}
+		neighbors.put(neighborNode, new Integer(weight));
+	}
+	
+	
+	public void removeNeighbor(Node neighborNode) {
+		if (neighbors.containsKey(neighborNode)) {
+			neighbors.remove(neighborNode);
+			neighborNode.inDegree--;
+		}
+	}
+	
+	
+	public boolean hasNeighbor(Node neighborNode) {
+		return neighbors.containsKey(neighborNode);
+	}
+	
+	
+	public int getInDegree() {
+		return inDegree;
+	}
+	
+	
+	@Override
+	public String toString() {
+		String retString = "Node: " + value;
+		Node[] neighborsArray = new Node[neighbors.size()];
+		neighbors.keySet().toArray(neighborsArray);
+		
+		for (int i = 0; i < neighborsArray.length; i++)
+			if (i == 0)
+				retString += "\nNeighbor(s): " + neighborsArray[i].getValue() + "," + neighbors.get(neighborsArray[i]);
+			else
+				retString += "; " + neighborsArray[i];
+		
+		return retString;
+	}
+}
