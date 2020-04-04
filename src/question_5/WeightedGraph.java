2bc0680b85e8c44b614db22f848324a1807569b5
bsc24 <bchin08@gmail.com>
2020-04-03 22:38:05 -0400

    Update WeightedGraph.java
    Fixed addWeightedEdge to allow for edges to go to from one Node to another Node which has an edge to the first Node.
    Added that each Node would be provided the set of all Nodes in the graph when created.
    Added method to get distance from one Ndoe to another.

--- src/question_5/WeightedGraph.java
+++ src/question_5/WeightedGraph.java
@@ -3,7 +3,6 @@ package question_5;
 import java.util.HashMap;
 import java.util.HashSet;
 
-import question_4.Node;
 
 public class WeightedGraph {
 	HashMap<String, Node> graphNodes;
@@ -18,7 +17,7 @@ public class WeightedGraph {
 		graphNodes = new HashMap<String, Node>();
 		setOfNodes = new HashSet<Node>();
 		
-		Node nodeToAdd = new Node(value);
+		Node nodeToAdd = new Node(value, setOfNodes);
 		
 		graphNodes.put(value, nodeToAdd);
 		setOfNodes.add(nodeToAdd);
@@ -29,7 +28,7 @@ public class WeightedGraph {
 		setOfNodes = new HashSet<Node>();
 		
 		for (int i = 0; i < values.length; i++) {
-			Node nodeToAdd = new Node(values[i]);
+			Node nodeToAdd = new Node(values[i], setOfNodes);
 			graphNodes.put(values[i], nodeToAdd);
 			setOfNodes.add(nodeToAdd);
 		}
@@ -37,16 +36,16 @@ public class WeightedGraph {
 	
 	
 	public void addNode(final String nodeVal) {
-		Node nodeToAdd = new Node(nodeVal);
+		Node nodeToAdd = new Node(nodeVal, setOfNodes);
 		
 		graphNodes.put(nodeVal, nodeToAdd);
 		setOfNodes.add(nodeToAdd);
 	}
 	
 	
-	public void addWeightedEdge(final Node first, final Node second, int weight) throws Exception {
-		if (graphNodes.containsValue(first) && graphNodes.containsValue(second) && !second.hasNeighbor(first)) {
-			first.setNeighbor(second);
+	public void addWeightedEdge(final Node first, final Node second, final int edgeWeight) throws Exception {
+		if (graphNodes.containsValue(first) && graphNodes.containsValue(second)) {
+			first.setNeighbor(second, edgeWeight);
 		}
 		else
 			throw new Exception("A provided node does not exist in the graph.");
@@ -61,8 +60,18 @@ public class WeightedGraph {
 			throw new Exception("A provided node does not exist in the graph.");
 	}
 	
+	
 	public boolean directedEdgeExists(final Node first, final Node second) {
 		return first.hasNeighbor(second);
+	}
+	
+	
+	public int distanceToNeighbor(final Node first, final Node second) throws Exception {
+		if (graphNodes.containsValue(first) && graphNodes.containsValue(second)) {
+			return first.distanceToNeighbor(second);
+		}
+		else
+			throw new Exception("A provided node does not exist in the graph.");
 	}
 	
 	
