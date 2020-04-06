19b3a781e065ba5d6daed4435baf768b0926870f
bsc24 <bchin08@gmail.com>
2020-04-05 23:11:55 -0400

    Update Question6MainTests.java
    Wrote tests for Question 6's Main.

--- src/testers/Question6MainTests.java
+++ src/testers/Question6MainTests.java
@@ -2,6 +2,8 @@ package testers;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import question_6.GridGraph;
@@ -13,9 +15,29 @@ public class Question6MainTests {
 	@Test		// Doesn't fully test automatically, the printed output must be manually checked, prints in no particular order
 	public void createRandomGridGraphTest() throws Exception {
 		GridGraph aGraph = Main.createRandomGridGraph(4);
-		
+
+		System.out.println("Test: createRandomGridGraphTest");
 		for (GridNode value: aGraph.getAllNodes())
 			System.out.println(value + "\n");
+	}
+	
+	
+	@Test		// Doesn't fully test automatically, the printed output must be manually checked, prints in no particular order
+	public void astarTest() throws Exception {
+		GridGraph aGraph = Main.createRandomGridGraph(4);
+		GridNode source = aGraph.getNode(0, 0);
+		GridNode destination = aGraph.getNode(4, 4);
+		
+		ArrayList<GridNode> astarAnswer = Main.astar(source, destination);
+
+		System.out.println("Test: astarTest");
+		System.out.println("Full graph: ");
+		for (GridNode value: aGraph.getAllNodes())
+			System.out.println(value + "\n");
+		
+		System.out.print("A* Traversal: ");
+		for (GridNode value: astarAnswer)
+			System.out.print(value.getValue() + " ");
 	}
 
 }
